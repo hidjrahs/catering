cbfad63e9a842222f2946604641582b7c7e2a9a0
Hidjrah Saputra <hijrah2005@gmail.com>
2026-06-27 15:42:51 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,12 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,12 +28,30 @@ class AppServiceProvider extends ServiceProvider
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
-        if(config('app.redirect_https') == true || config('app.redirect_https') === 'true') {
-            $url->forceScheme('https');
+
+        Request::setTrustedProxies(
+            ['127.0.0.1', '::1'],
+            SymfonyRequest::HEADER_X_FORWARDED_FOR
+            | SymfonyRequest::HEADER_X_FORWARDED_HOST
+            | SymfonyRequest::HEADER_X_FORWARDED_PORT
+            | SymfonyRequest::HEADER_X_FORWARDED_PROTO
+        );
+
+        $request = $this->app['request'];
+        if ($request->hasHeader('X-Forwarded-Proto')) {
+            $request->server->set('HTTPS', $request->header('X-Forwarded-Proto') === 'https');
         }
-        if ($this->app->environment('production')) {
-        URL::forceScheme('https');
-    }
+
+        $shouldForceHttps = config('app.redirect_https') == true
+            || config('app.redirect_https') === 'true'
+            || $this->app->environment('production')
+            || $request->isSecure();
+
+        if ($shouldForceHttps) {
+            $url->forceScheme('https');
+            URL::forceScheme('https');
+        }
+
         // Schema::defaultStringLength(191);
     }
 }
